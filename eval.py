efbc0205eee0352e57f7c6f1da95e1ab604aa8ea
kie4280 <kie4280@livemail.tw>
2022-05-08 10:33:48 -0500

    add padding in eval

--- eval.py
+++ eval.py
@@ -9,25 +9,23 @@ from tqdm import tqdm
 physical_devices = tf.config.list_physical_devices('GPU')
 model = tf.keras.models.load_model("checkpoints/model-ep40.pth/")
 
-dataset = DIV2KDataset(config.DIV2K_PATH, 'train')
+dataset = DIV2KDataset(config.DIV2K_PATH, 'valid')
 train_data = tf.data.Dataset.from_generator(
     dataset.pair_generator,
     output_signature=(tf.TensorSpec((None, None, 3)), tf.TensorSpec((None, None, 3))))
 # train_data = train_data.batch(config.BATCH_SIZE) # cannot batch because of different image size
-train_data = train_data.shuffle(2)
-plt.figure()
-prog = tqdm(range(config.EPOCHS))
-en = train_data.enumerate()
+en = tqdm(train_data.enumerate())
 for step, (X_train, y_train) in en:
 
 
-    #     # batch size of 1
+    # batch size of 1
     X_train:Tensor = tf.expand_dims(X_train, 0)
-        
+    H, W, _ = X_train.shape
+    H, W = H + 8 - (H % 8), W + 8 - (W % 8)
+    X_train = tf.image.pad_to_bounding_box(X_train,0,0, H, W)
     output = model(X_train, training=False)
     output = tf.clip_by_value(output[0]/255, 0, 1)
     plt.imsave(f"outputs/{step}.png", output.numpy())
-    prog.set_postfix({"step":step})
    
 
 
